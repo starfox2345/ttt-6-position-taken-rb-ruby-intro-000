5c8ff595306734c44be2f4e0823a72b92a496e3f
Yat Sun Lam <yatsunlam25@gmail.com>
2020-10-14 04:42:24 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,13 +5,13 @@ def position_taken?(board, index)
 #  index = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   index = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-  if board[index] == " " || board[index] == ""
-    return false
+#  if board[index] == " " || board[index] == ""
+#    return false
 #  elsif board[index] == nil
 #    return false
 #  elsif board[index] == "X" || board[index]  == "O"
 #    return true
 #  else
 #    nil
-  end
+#  end
 end
