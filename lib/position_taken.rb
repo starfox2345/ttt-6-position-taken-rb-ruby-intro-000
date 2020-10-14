fd52dbae96425577f1519474146ca4c07ea79dcb
Yat Sun Lam <yatsunlam25@gmail.com>
2020-10-14 04:00:04 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,11 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  index = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+#  index = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  index = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-#  if board[index] == " " || board[index] == ""
-#    return false
+  if board[index] == " " || board[index] == ""
+    return false
 #  elsif board[index] == nil
 #    return false
 #  elsif board[index] == "X" || board[index]  == "O"
