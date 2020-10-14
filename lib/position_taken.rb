7d2d1aead730504d076674a05700984883dc6ec4
Yat Sun Lam <yatsunlam25@gmail.com>
2020-10-14 04:26:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,11 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-#  index = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  index = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  index = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+#  index = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  puts "Which position would you like to take?"
+
 
   if board[index] == " " || board[index] == ""
     return false
